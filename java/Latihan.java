2d4dfd68c7565acff1cef988764b9b1f2572ba67
kevinrimbun03 <kevinrimbun03>
2022-11-07 18:31:00 +0700

    replace .sort method

--- java/Latihan.java
+++ java/Latihan.java
@@ -5,8 +5,7 @@ public class Latihan {
 
         System.out.println("---Soal No.1---");
         Scanner object = new Scanner(System.in);
-        // System.out.println("Enter Your First Name, Last Name, Birth Place,Fav Programming Language, and your Birth Year: ");
-
+       
         System.out.println("Enter Your First Name: ");
         String firstName = object.nextLine();
         System.out.println("Enter Your Last Name: ");
@@ -44,13 +43,25 @@ public class Latihan {
         System.out.println(" ");
         System.out.println("\n---Soal No.3---");
         int[] array = {3,2,4,1,5};
-        Arrays.sort(array);
-
+        // Arrays.sort(array);
         System.out.println("Urutan dari data array diatas adalah:");
-
-        for(int i = 0; i < array.length; i++){
-            System.out.print(array[i] + ", ");
+        int index = -1;
+        for(int i = 0; i < array.length; i++) {
+            index = i;
+            for(int j = i; j <= array.length-1 ; j++) {
+                if (array[j] < array[index]) {
+                    index =j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+            
+        } for(int i = 0; i <array.length; i++) {
+            System.out.print(array[i] + ",");
         }
+
+        
 
         
     }
